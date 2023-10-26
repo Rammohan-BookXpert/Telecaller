2c07c9b3d2aa33e72611cc4d8f65ff68a1cd5053
rammohan-bookxpert <rammohan.bookxpert@gmail.com>
2023-10-27 05:27:36 +0530

    Initial Commit v1.0.0

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    // Safe Args
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.fairsoft.telecaller"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.fairsoft.telecaller"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Navigation Component
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
+
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.fragment:fragment-ktx:1.6.1")
+
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    // Lifecycle extensions
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    // Easy Permissions
+    implementation ("com.vmadalin:easypermissions-ktx:1.0.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Gson Retrofit
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // SwipeRefreshLayout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/fairsoft/telecaller/fragments/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.fairsoft.telecaller.fragments
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+}
