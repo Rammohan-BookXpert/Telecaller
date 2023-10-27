218ea5dc1d6b35d9a22ddf3fd9c793ff32bca603
rammohan-bookxpert <rammohan.bookxpert@gmail.com>
2023-10-27 05:35:35 +0530

    Initial Commit v1.0.0

--- app/src/main/java/com/fairsoft/telecaller/MainActivity.kt
+++ app/src/main/java/com/fairsoft/telecaller/MainActivity.kt
@@ -2,10 +2,29 @@ package com.fairsoft.telecaller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.fairsoft.telecaller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Getting NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+    }
+
+    // This will take care of Back navigation actions
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
